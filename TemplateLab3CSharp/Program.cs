e47991ed564e18aebf214eeb532b3fab7756e4d6
ob3r0n <ob3r0n@polisan.ddns.net>
2022-11-13 18:36:25 +0200

    Add lab3

--- TemplateLab3CSharp/Program.cs
+++ TemplateLab3CSharp/Program.cs
@@ -1,97 +1,222 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MyTask;
+﻿using System;
 
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-/// 
-Console.WriteLine("Hello, World!");
-//
-//  Приклад коду
-//
-
-Animal[] animals = new Animal[4];
-animals[0] = new Animal();
-animals[0].Name = "Bizon"; animals[0].WWeight=100;
-Console.WriteLine(" my animal " + animals[0].ToString());
-animals[1] = new Animal(100,10,"Cow");
-animals[2] = new Animal(102, 12, "Cow Big");
-animals[3] = new Animal(100, 10, "Zebra");
-
-for(int i = 0; i < 4; i++)
+namespace Program
 {
-    Console.WriteLine("  animal " + i + " " + animals[i].ToString());
-}
-
-Big b = new Big();
-Console.WriteLine(" Big" + b +"   "+ b.ToString() + "  " + b.Name);
-
-Console.WriteLine(" Task 2");
-Task2.Run();
-     
-
-/// <summary>
-///  Закічення  іструкцій верхнього рівня
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-///   
-/// </summary>
-class Big
-{
-    private string name =" First init";
-    public Big()
+    class Trapeze
     {
-        name = "NoName";
-    }
-    public Big(string name)
-    {
-        this.name = name;
-        
-    }
-    public string Name { 
-        get { return name; } 
-        set { name = value; }
-    }
+        private int a, b, c, d;
+        private readonly int color;
 
-};
-namespace MyTask
-{
-    static class Task2
+        public Trapeze(int a, int b, int c, int d, int color)
         {
-       public  static void Run()
-        {
-            Drv d = new Drv("RRR");
-            Console.WriteLine(" rrr  " + d.ToString());
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.color = color;
         }
-        }
-    class Base
-    {
-        string NameBase;
-        protected string any_Inform;
-        int type = 0;
-        public int numobj = 0;
-        public Base() {
-            NameBase = "Base";
-            any_Inform = "";
 
+        public void Print()
+        {
+            Console.WriteLine($"A: {a}\nB: {b}\nC: {c}\nD: {d}\nColor: {color}");
         }
+
+        public double P()
+        {
+            return a + b + c + d;
+        }
+
+        public double S()
+        {
+            double p = P() / 2;
+            return ((a + b) / (Math.Abs(a - b) * Math.Sqrt((p - a) * (p - b) * (p - a - c) * (p - a - d))));
+        }
+
+        public bool IsSquare()
+        {
+            if (a == b && b == c && c == d) return true;
+            return false;
+        }
+
+        public int getA()
+        {
+            return a;
+        }
+        public int getB()
+        {
+            return b;
+        }
+        public int getC()
+        {
+            return c;
+        }
+        public int getD()
+        {
+            return d;
+        }
+        public int getColor()
+        {
+            return color;
+        }
+
+        public void setA(int newA)
+        {
+            a = newA;
+        }
+        public void setB(int newA)
+        {
+            b = newA;
+        }
+        public void setC(int newA)
+        {
+            c = newA;
+        }
+        public void setD(int newA)
+        {
+            d = newA;
+        }
+
+    }
+
+    class Test
+    {
+        public string question;
+
+        public Test()
+        {
+            question = "none";
+        }
+
+        public Test(string quest)
+        {
+            question = quest;
+        }
+
         public void Show()
         {
-            Console.WriteLine(" Show Base " + NameBase);
+            Console.WriteLine($"Question: {question}");
         }
-    };
+    }
 
-    class Drv :  Base
+    class Ispyt : Test
     {
-        Drv()
+        private double maxOcinka;
+        public Ispyt()
         {
-            numobj = 10;
+            question = "none";
+            maxOcinka = 0;
         }
-       public Drv(string any_Inform)
+
+        public Ispyt(double ocinka, string quest)
         {
-            numobj = 11;
-            this.any_Inform = any_Inform;
+            maxOcinka = ocinka;
+            question = quest;
+        }
+
+        public Ispyt(double ocinka)
+        {
+            maxOcinka = ocinka;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Question: {question}");
+            Console.WriteLine($"Max ocinka: {maxOcinka}");
+        }
+    }
+
+    class VipyskniyIspyt : Test
+    {
+        private double prohidnyi;
+        public VipyskniyIspyt()
+        {
+            question = "none";
+            prohidnyi = 0;
+        }
+
+        public VipyskniyIspyt(double ocinka, string quest)
+        {
+            prohidnyi = ocinka;
+            question = quest;
+        }
+
+        public VipyskniyIspyt(double ocinka)
+        {
+            prohidnyi = ocinka;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Question: {question}");
+            Console.WriteLine($"Prohidnyi: {prohidnyi}");
+        }
+    }
+
+    class Vyprobyvannya : Test
+    {
+        private double prohidnyi;
+        public Vyprobyvannya()
+        {
+            question = "none";
+            prohidnyi = 0;
+        }
+
+        public Vyprobyvannya(double ocinka, string quest)
+        {
+            prohidnyi = ocinka;
+            question = quest;
+        }
+
+        public Vyprobyvannya(double ocinka)
+        {
+            prohidnyi = ocinka;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Question: {question}");
+            Console.WriteLine($"Ocinka: {prohidnyi}");
+        }
+    }
+
+
+
+    static class Program
+    {
+        static void Main()
+        {
+            var arr = new List<Trapeze> {
+             new Trapeze(1, 1, 1, 1, 1),
+             new Trapeze(11, 15, 4, 4, 2),
+             new Trapeze(2, 6, 6, 3, 3),
+             new Trapeze(4, 14, 15, 6, 1),
+             new Trapeze(2, 2, 2, 2, 2)
+            };
+            int squares = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                Console.WriteLine($"Trapezia {i}");
+                Console.WriteLine($"P: {arr[i].P()}");
+                Console.WriteLine($"S: {arr[i].S()}");
+                Console.WriteLine($"Color: {arr[i].getColor()}");
+                Console.WriteLine($"isSquare: {arr[i].IsSquare()}");
+                if (arr[i].IsSquare()) squares++;
+            }
+            Console.WriteLine($"Squares: {squares}");
+            Console.WriteLine();
+
+            Test test = new Test("test");
+            test.Show();
+            Console.WriteLine();
+            Ispyt ispyt = new Ispyt(20, "ispyt");
+            ispyt.Show();
+            Console.WriteLine();
+            VipyskniyIspyt vipyskniyIspyt = new VipyskniyIspyt(30, "vipysk");
+            vipyskniyIspyt.Show();
+            Console.WriteLine();
+            Vyprobyvannya vyprobyvannya = new Vyprobyvannya(5, "vyprobyvannya");
+            vyprobyvannya.Show();
+            Console.WriteLine();
         }
     }
 }
